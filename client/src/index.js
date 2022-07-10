dfd2f9af0d33f233f98adb4bc71800c34a874852
Hunter McGrew <hunter109@gmail.com>
2022-07-10 15:01:52 -0500

    changed file structure

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import 'bulma/css/bulma.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
